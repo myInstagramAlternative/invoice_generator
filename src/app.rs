7cc111695d6ab7d08d2bb9d9aac2b1951b93ecff
Petar Babic <jebachnakvadrat@gmail.com>
2022-08-31 15:35:42 +0200

    doing stuff

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,12 @@
+use std::path::Path;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     // Example stuff:
     label: String,
-
+    tester: String,
     // this how you opt-out of serialization of a member
     #[serde(skip)]
     value: f32,
@@ -16,6 +18,7 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            tester: "Hello World!".to_owned(),
         }
     }
 }
@@ -25,6 +28,29 @@ impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        let mut fonts = egui::FontDefinitions::default();
+
+        // Install my own font (maybe supporting non-latin characters):
+        fonts.font_data.insert(
+            "my_font".to_owned(),
+            egui::FontData::from_static(include_bytes!("../assets/moderna.TTF")),
+        ); // .ttf and .otf supported
+
+        // Put my font first (highest priority):
+        fonts
+            .families
+            .get_mut(&egui::FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "my_font".to_owned());
+        //
+        // // Put my font as last fallback for monospace:
+        fonts
+            .families
+            .get_mut(&egui::FontFamily::Monospace)
+            .unwrap()
+            .push("my_font".to_owned());
+
+        cc.egui_ctx.set_fonts(fonts);
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
@@ -36,6 +62,49 @@ impl TemplateApp {
     }
 }
 
+struct MyImage {
+    texture: Option<egui::TextureHandle>,
+}
+
+impl MyImage {
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        let texture: &egui::TextureHandle = self.texture.get_or_insert_with(|| {
+            // Load the texture only once.
+            ui.ctx().load_texture(
+                "my-image",
+                egui::ColorImage::example(),
+                egui::TextureFilter::Linear
+            )
+        });
+
+        // Show the image:
+        ui.image(texture, texture.size_vec2());
+    }
+
+}
+
+pub fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
+    let image = image::io::Reader::open(path)?.decode()?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
+}
+
+pub fn load_image_from_memory(image_data: &[u8]) -> Result<egui::ColorImage, image::ImageError> {
+    let image = image::load_from_memory(image_data)?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
+}
+
 impl eframe::App for TemplateApp {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
@@ -45,7 +114,11 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            label,
+            value,
+            tester,
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -55,12 +128,55 @@ impl eframe::App for TemplateApp {
         #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
+
             egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        _frame.close();
-                    }
-                });
+                ui.heading(
+                    egui::RichText::new("Invoice")
+                        .size(35.0_f32)
+                        .color(egui::Color32::GREEN),
+                );
+                //let mut my_value = 0.0_f32;
+                //ui.add_sized([40.0, 20.0], egui::DragValue::new(&mut my_value));
+
+                ui.menu_button(
+                    egui::RichText::new("generator")
+                        .size(30.0_f32)
+                        .color(egui::Color32::DARK_GREEN),
+                    |ui| {
+                        ui.horizontal(|ui| {
+                            let generator_btn = ui.button(
+                                egui::RichText::new("Quit")
+                                    .size(25.0_f32)
+                                    .color(egui::Color32::LIGHT_GRAY),
+                            );
+            
+  
+                            if generator_btn.clicked() {
+                                _frame.close();
+                            }
+                            if generator_btn.hovered() {
+                                // generator_btn.set_color(egui::Color32::LIGHT_GRAY);
+                                load_image_from_path(Path::new("assets/icon-256.png"))
+                                    .map(|image| {
+                                        let mut my_image = MyImage { texture: None };
+                                        my_image.texture = Some(ui.ctx().load_texture(
+                                            "my-image",
+                                            image,
+                                            egui::TextureFilter::Linear,
+                                        ));
+                                        my_image.ui(ui);
+                                    })
+                                    .unwrap_or_else(|err| {
+                                        panic!("Failed to load image: {:?}", err);
+                                        //egui::log_error!("Failed to load image: {}", err);
+                                    });
+                               
+                            } else {
+                                //   generator_btn.set_color(egui::Color32::DARK_GREEN);
+                            }
+                        });
+                    },
+                );
             });
         });
 
@@ -72,6 +188,13 @@ impl eframe::App for TemplateApp {
                 ui.text_edit_singleline(label);
             });
 
+            let response = ui.add(egui::TextEdit::singleline(&mut tester.clone()));
+            if response.changed() {
+                // …
+            }
+            if response.lost_focus() && ui.input().key_pressed(egui::Key::Enter) {
+                // …
+            }
             ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
             if ui.button("Increment").clicked() {
                 *value += 1.0;
